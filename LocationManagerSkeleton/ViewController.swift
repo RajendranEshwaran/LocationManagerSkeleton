7dea92feebba3f76af82278a17ff9fa50922ac89
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-01-28 10:39:10 -0500

    Initial Commit

---
+++ LocationManagerSkeleton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationManagerSkeleton
+//
+//  Created by gomathi saminathan on 1/28/20.
+//  Copyright © 2020 Rajendran Eshwaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
